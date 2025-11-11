e0b3d93fe630ba06d072ff265d6ef9e2fed00a29
Manish <itsmenishan.12@gmail.com>
2025-11-11 09:24:56 +0530

    print till n without iteration

--- src/main/java/org/example/recursion/PrintNumToN.java
+++ src/main/java/org/example/recursion/PrintNumToN.java
@@ -1,4 +1,16 @@
 package org.example.recursion;
 
 public class PrintNumToN {
+
+    public static void main(String[] args) {
+        printNos(100);
+    }
+
+    public static void printNos(int n) {
+        if(n==0){
+            return;
+        }
+        printNos(n-1);
+        System.out.print(n+" ");
+    }
 }
